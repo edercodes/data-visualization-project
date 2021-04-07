f7a520ef357631b6e664c603e7375c6e6947264b
calderoneder <64505582+calderoneder@users.noreply.github.com>
2021-04-07 10:11:50 -0700

    Update eq_explore_data.py

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -7,9 +7,15 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags = []
+mags, lons, lats = [], [], []       ### list is made and then loops through dictionary all_eq_dicts
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
+    mag = eq_dict['properties']['mag']      ### each EQ is stored in 'properties' section of this dictionary and under the key 'mag'
+    lon = eq_dict['geometry']['coordinates'][0]     ### accesses dictionary representing the geometry element in the EQ
+    lat = eq_dict['geometry']['coordinates'][1]     ### second key pulls the list of values associated with coordinates
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
