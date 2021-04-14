39f4f17aeec74884034991750802546883299786
edercodes <64505582+edercodes@users.noreply.github.com>
2021-04-14 15:07:50 -0700

    Update hn_submissions.py

--- hn_submissions.py
+++ hn_submissions.py
@@ -3,23 +3,23 @@ from operator import itemgetter
 import requests
 
 # Make an API call and store the respoonse.
-url = 'https://hacker-news.firebaseio.com/v0/topstories.json'       ### make an API call and print status of response
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json' 
 r = requests.get(url)
 print(f"Status code: {r.status_code}")
 
 # Process information about each submission.
-submission_ids = r.json()       ### convert the response object to a Python list and store in submission_ids
-submission_dicts = []       ### set up an empty list to store these dictionaries
+submission_ids = r.json()
+submission_dicts = [] 
 for submission_id in submission_ids[:30]:
     # Make a seperate API call for each submission.
-    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"        ### make a new API call generating a URL that includes current value of submission_id
+    url = f"https://hacker-news.firebaseio.com/v0/item/{submission_id}.json"    
     r = requests.get(url)
     print(f"id: {submission_id}\tstatus: {r.status_code}")
     response_dict = r.json()
 
     # Build a dictionary for each article.
     try:
-        submission_dict = {     ### create a dictionary to store title, links, and comment of articles received
+        submission_dict = {  
             'title': response_dict['title'],
             'hn_link': f"http://news.ycombinator.com/item?id={submission_id}",
             'comments': response_dict['descendants'],
@@ -29,7 +29,7 @@ for submission_id in submission_ids[:30]:
         continue
 
     else:
-        submission_dicts.append(submission_dict)        ### resulting list is appended to submission_dicts
+        submission_dicts.append(submission_dict) 
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                             reverse=True)
