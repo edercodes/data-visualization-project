3b47e1d0edc8e7eec80e9539da1743e61f3145db
edercodes <64505582+edercodes@users.noreply.github.com>
2021-04-14 14:58:01 -0700

    Update death_valley_highs_lows.py

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -12,12 +12,12 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-        try:        ### date, high & low temps are extracted each time a row is examined
+        try:
             high = int(row[4])
             low = int(row[5])
-        except ValueError:      ### Python raises ValueError when info is missing
-            print(f"Missing data for {current_date}")       ### loop continues processing next row after printing error
-        else:       ### if there is no error, the else block will run and data will be appended
+        except ValueError:
+            print(f"Missing data for {current_date}")       
+        else:    
             dates.append(current_date)
             highs.append(high)
             lows.append(low)
