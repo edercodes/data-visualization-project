a5c18686eae7a7caa9952c9abbc5ec457bcccf1c
edercodes <64505582+edercodes@users.noreply.github.com>
2021-04-14 15:35:23 -0700

    Update mpl_squares.py

--- mpl_squares.py
+++ mpl_squares.py
@@ -4,8 +4,8 @@ input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
 plt.style.use('seaborn')
-fig, ax = plt.subplots()    ### generates one or more plots in the same figure
-ax.plot(input_values, squares, linewidth=3)   ### controls thickness of line that plot() generates
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth=3)
 
 # Set chart title and level axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -13,6 +13,6 @@ ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
-ax.tick_params(axis='both', labelsize=14)   ### tick_params() styles the tick marks
+ax.tick_params(axis='both', labelsize=14)
 
 plt.savefig('mpl.squares.png', bbox_inches='tight')
