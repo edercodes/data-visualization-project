91f3a8a520c152bf1f1d4d23bf8edb1044644f3f
edercodes <64505582+edercodes@users.noreply.github.com>
2021-04-14 15:46:12 -0700

    Update sitka_highs.py

--- sitka_highs.py
+++ sitka_highs.py
@@ -3,15 +3,15 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_2018_simple.csv'      ### open file, and assign to resulting file object to f
+filename = 'data/sitka_weather_2018_simple.csv'    
 with open(filename) as f:
-    reader = csv.reader(f)      ### call csv.reader, passes file as argument, and creates reader object associated with that file
-    header_row = next(reader)        ### calling next once gets first line of the file, which contains headers
+    reader = csv.reader(f)      
+    header_row = next(reader)       
 
     # Get dates, and high and low temperatures from this file.
-    dates, highs, lows = [], [], []     ### two lists are created to attain values from file
+    dates, highs, lows = [], [], [] 
     for row in reader:
-        current_date = datetime.strptime(row[2], '%Y-%m-%d')        ### data containing date info converted to datetime object
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
         low = int(row[6])
         dates.append(current_date)
@@ -21,13 +21,13 @@ with open(filename) as f:
 # Plot the high and low temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')     ### pass list of highs to to plot() and plot points in red
+ax.plot(dates, highs, c='red')
 ax.plot(dates, lows, c='blue')
 
 # Format plot.
 ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
-fig.autofmt_xdate()     ### draws date labels diagaonolly to prevent them from overlapping
+fig.autofmt_xdate() 
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
