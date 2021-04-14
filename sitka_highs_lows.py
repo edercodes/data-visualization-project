c1424572b186508c45359abe0fa91e44d0ac621d
edercodes <64505582+edercodes@users.noreply.github.com>
2021-04-14 15:47:35 -0700

    Update sitka_highs_lows.py

--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -3,15 +3,15 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'sitka_weather_2018_simple.csv'      ### open file, and assign to resulting file object to f
+filename = 'sitka_weather_2018_simple.csv'
 with open(filename) as f:
-    reader = csv.reader(f)      ### call csv.reader, passes file as argument, and creates reader object associated with that file
-    header_row = next(reader)        ### calling next once gets first line of the file, which contains headers
+    reader = csv.reader(f)
+    header_row = next(reader)
 
     # Get dates, and high and low temperatures from this file.
-    dates, highs, lows = [], [], []     ### three lists are created to attain values from file
+    dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime(row[2], '%Y-%m-%d')        ### data containing date info converted to datetime object
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
         low = int(row[6])
         dates.append(current_date)
@@ -21,14 +21,14 @@ with open(filename) as f:
 # Plot the high and low temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red', alpha=0.5)     ### pass list of highs to to plot() and plot points in red, call plots low temps and colors them blue
-ax.plot(dates, lows, c='blue', alpha=0.5)      ### alpha argument controls a colors transparency
-ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)        ### facecolor argument determines color of shaded region
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
 ax.set_title("Daily high and low temperatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
-fig.autofmt_xdate()     ### draws date labels diagaonolly to prevent them from overlapping
+fig.autofmt_xdate() 
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(axis='both', which='major', labelsize=16)
 
